23ab220df26f4a1f401a9bf053d9108c3555bb44
Tim Sotirhos <tim.sotirhos@gmail.com>
2019-09-06 08:56:43 -0500

    sql joins

--- 3.8.2_join_exercises.sql
+++ 3.8.2_join_exercises.sql
@@ -82,14 +82,36 @@ Order by departments.dept_name asc;
 
 --6) Find the number of employees in each department.
 
+SELECT departments.dept_no, departments.dept_name, count(*) AS num_employees
+FROM departments
+JOIN dept_emp ON (departments.dept_no = dept_emp.dept_no)
+JOIN employees ON (dept_emp.emp_no = employees.emp_no)
+WHERE dept_emp.to_date = '9999-01-01'
+GROUP BY departments.dept_no, departments.dept_name;
 
 
 --7) Which department has the highest average salary?
 
+SELECT departments.dept_name, AVG(salaries.salary) AS average_salary
+FROM departments
+JOIN dept_emp ON (departments.dept_no = dept_emp.dept_no)
+JOIN employees ON (dept_emp.emp_no = employees.emp_no)
+JOIN salaries ON (employees.emp_no = salaries.emp_no)
+WHERE dept_emp.to_date = '9999-01-01' AND salaries.to_date = '9999-01-01'
+GROUP BY departments.dept_name
+ORDER BY average_salary DESC
+LIMIT 1;
 
 --8) Who is the highest paid employee in the Marketing department?
 
-
+SELECT employees.first_name, employees.last_name
+FROM departments
+JOIN dept_emp ON (departments.dept_no = dept_emp.dept_no)
+JOIN employees ON (dept_emp.emp_no = employees.emp_no)
+JOIN salaries ON (employees.emp_no = salaries.emp_no)
+WHERE dept_emp.to_date = '9999-01-01' AND salaries.to_date = '9999-01-01' AND departments.dept_name = 'Marketing'
+ORDER BY salaries.salary DESC
+LIMIT 1;
 
 --9) Which current department manager has the highest salary?
 
