1d36456c95127f7e50d2943bfe50316e51c9e3e9
Tim Sotirhos <tim.sotirhos@gmail.com>
2019-09-07 17:10:53 -0500

    sql databases

---
+++ 3.2_databases_exercises.sql
@@ -0,0 +1,25 @@
+/*
+
+--1) Connect to the database server
+
+--2) List all the databases
+
+SHOW databases;
+
+--3) Switch to a database using the USE statement
+
+USE employees;
+
+--4) Show the currently selected database
+
+SELECT database();
+
+--5) Switch to a different database
+
+USE sakila;
+
+--6) Show the currently selected database
+
+SELECT database();
+
+*/
